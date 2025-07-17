6b909f189919e45c01b5dc6d309d655a7fbb3cba
vinodhini4667 <vinodhini4667@gmail.com>
2025-07-17 20:50:50 +0530

    Create DailyTemperatures.java

---
+++ Stack/DailyTemperatures.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n=temperatures.length;
+        int[] answer=new int[n];
+        Stack<Integer> stack=new Stack<>();
+
+        for(int i=0;i<n;i++)
+        {
+            while(!stack.isEmpty() && temperatures[i]>temperatures[stack.peek()])
+            {
+                int previndex=stack.pop();
+                answer[previndex]=i-previndex;
+            }
+            stack.push(i);
+        }
+        return answer;
+    }
+}
