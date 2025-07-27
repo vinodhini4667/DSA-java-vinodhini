c0fa2d763a392d08bb5d452ba74c490b5f30e98f
vinodhini4667 <vinodhini4667@gmail.com>
2025-07-27 18:36:26 +0530

    Create PartitionList.java

---
+++ LinkedList/PartitionList.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode beforehead=new ListNode(0);
+        ListNode afterhead=new ListNode(0);
+        ListNode before=beforehead;
+        ListNode after=afterhead;
+
+        while(head!=null)
+        {
+            if(head.val<x)
+            {
+                before.next=head;
+                before=before.next;
+            }
+            else
+            {
+                after.next=head;
+                after=after.next;
+            }
+            head=head.next;
+        }
+        after.next=null;
+        before.next=afterhead.next;
+
+        return beforehead.next;
+    }
+}
