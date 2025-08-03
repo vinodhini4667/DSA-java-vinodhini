025225328c19fb5975afd133d67112a80a397aa4
vinodhini4667 <vinodhini4667@gmail.com>
2025-08-03 09:53:19 +0530

    Create NumberofIslands.java

---
+++ Graphs/NumberofIslands.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        if(grid==null || grid.length==0) return 0;
+
+        int count=0;
+        for(int i=0;i<grid.length;i++)
+        {
+            for(int j=0;j<grid[0].length;j++)
+            {
+                if(grid[i][j]=='1')
+                {
+                    dfs(grid,i,j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    private void dfs(char[][] grid,int i,int j)
+    {
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]=='0')
+        return;
+
+        grid[i][j]='0'; //mark visited
+
+        dfs(grid,i+1,j); //down
+        dfs(grid,i-1,j); //up
+        dfs(grid,i,j+1); //right
+        dfs(grid,i,j-1);
+
+    }
+}
